9eb155a4a2a98cbc010bd08fdd7f9f68839199b4
ThatKidFlo <Floryn2009@windowslive.com>
2016-05-22 12:21:48 +0300

    Level v0.2

---
+++ Assets/Drugs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+using System.Collections;
+
+public class Drugs : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Camera.FindObjectOfType<Camera>().GetComponent<VignetteAndChromaticAberration>().chromaticAberration = 15.5f;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/OwnAssets/Scripts/StopFollowing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityStandardAssets._2D;
+using System.Collections;
+
+public class StopFollowing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
